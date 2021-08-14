fd7ead8f228cce757c974f3ce8e4981ed38475bf
also404t <529220735@qq.com>
2021-08-14 16:09:31 +0800

    Site updated: 2021-08-14 16:09:27

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,6 +1,6 @@
 // 代码块语言识别
 
-$(function () {
+/*$(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
 
   $('pre').before($highlight_lang);
@@ -17,4 +17,25 @@ $(function () {
     
     $(this).siblings(".code_lang").text(lang_name);
   });
+});*/
+
+
+$(function () {
+  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
+
+  $('pre').before($highlight_lang);
+  $('pre').each(function () {
+    var code_language = $(this).attr('class');
+
+    if (!code_language) {
+      return true;
+    };
+    var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
+
+    // 首字母大写
+    // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
+
+    $(this).siblings(".code_lang").text(lang_name);
+  });
 });
+
